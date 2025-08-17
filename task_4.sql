94c6cf3b073f01dac386bad7ce0c1ba84438bccc
Blazious <blaziousmwambuwa@gmail.com>
2025-08-17 13:28:58 +0300

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
--- Print full description of the table books
+-- Print full description of the table Books
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-  AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
