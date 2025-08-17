3a0dcaa36b7a272cf5902bc61014de9e99126a26
Blazious <blaziousmwambuwa@gmail.com>
2025-08-17 13:00:36 +0300

    first commit

---
+++ MySQLServer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
+
+# Load environment variables from .env
+load_dotenv()
+
+def create_database():
+    try:
+        # connect to MySQL server using env vars
+        connection = mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST", "localhost"),
+            user=os.getenv("MYSQL_USER", "root"),
+            password=os.getenv("MYSQL_PASSWORD", "")
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error while connecting to MySQL: {e}")
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+
+if __name__ == "__main__":
+    create_database()
