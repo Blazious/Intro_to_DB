836fa3321d7d41fcbfefd6cfa756fc8830cdd34d
Blazious <blaziousmwambuwa@gmail.com>
2025-08-17 13:31:42 +0300

    task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- Script to insert a single customer record
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
